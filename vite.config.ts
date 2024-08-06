a38d204a6204c9695b867997d592d311987f8626
cao-qh <380229607@qq.com>
2024-08-06 14:40:48 +0800

    运行使用本地ip，临时取消代理服务器

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 import path from 'path'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
@@ -10,9 +11,10 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+// export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // 获取各种环境下的对应的变量
-  const env = loadEnv(mode, process.cwd())
+  // const env = loadEnv(mode, process.cwd())
   return {
     plugins: [
       vue(),
@@ -51,16 +53,16 @@ export default defineConfig(({ mode }) => {
     // 代理跨域
     server: {
       port: 5174,
-      proxy: {
-        [env.VITE_APP_BASE_API]: {
-          // 获取数据的服务器地址设置
-          target: env.VITE_SERVE,
-          // 需要代理跨域
-          changeOrigin: true,
-          // 路径重写
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
+      // proxy: {
+      //   [env.VITE_APP_BASE_API]: {
+      //     // 获取数据的服务器地址设置
+      //     target: env.VITE_SERVE,
+      //     // 需要代理跨域
+      //     changeOrigin: true,
+      //     // 路径重写
+      //     rewrite: (path) => path.replace(/^\/api/, ''),
+      //   },
+      // },
     },
   }
 })
