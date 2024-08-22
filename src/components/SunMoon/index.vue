4bac57cd1fb804476a727d6e85dfff84981dd704
cao-qh <380229607@qq.com>
2024-08-22 10:09:36 +0800

    切换暗黑模式的组件编写

--- src/components/SunMoon/index.vue
+++ src/components/SunMoon/index.vue
@@ -1,15 +1,40 @@
 <template>
-  <div class="sun-moon holder leftChecked">
-    <span class="icon">
-      <SvgIcon name="sun" />
+  <div class="sun-moon holder" :class="{ leftChecked }">
+    <span class="icon" @click="handleSun">
+      <SvgIcon name="sun" :color="iconColor.sun" />
     </span>
-    <span class="icon">
-      <SvgIcon name="moon" />
+    <span class="icon" @click="handleMoon">
+      <SvgIcon name="moon" :color="iconColor.moon" />
     </span>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import useLayoutSettingStore from '@/store/modules/setting'
+
+const settingStore = useLayoutSettingStore()
+
+const leftChecked = ref(!settingStore.dark)
+const iconColor = computed(() => {
+  return {
+    sun: settingStore.dark ? '#a3b1bf' : '#fff',
+    moon: settingStore.dark ? '#fff' : '#a3b1bf',
+  }
+})
+
+// 当太阳被点击
+const handleSun = () => {
+  leftChecked.value = true
+  settingStore.dark = false
+}
+
+// 当月亮被点击
+const handleMoon = () => {
+  leftChecked.value = false
+  settingStore.dark = true
+}
+</script>
 
 <style lang="scss" scoped>
 .sun-moon {
@@ -28,7 +53,7 @@
     line-height: 32px;
     text-align: center;
     transition: all 0.3s;
-    font-size: 16px;
+    font-size: 19px;
 
     /*  .icon.active {
       color: #fff;
