28150415049ab698bfe5065b1af189b3c47a6a7a
cao-qh <380229607@qq.com>
2024-09-04 14:39:54 +0800

    上传图片组件优化

--- src/components/UploadImage/index.vue
+++ src/components/UploadImage/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <a-upload
+      ref="upload"
       accept="image/png, image/jpeg"
       list-type="picture-card"
       :maxCount="1"
@@ -19,26 +20,39 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import type { UploadProps } from 'ant-design-vue'
 import file2base64 from '@/utils/file2base64'
 import ImageViewer from '@/components/ImageViewer/index.vue'
 
+defineOptions({
+  name: 'UploadImage',
+})
+
 const model = defineModel('value')
+const upload = ref()
+
+watch(model, (val) => {
+  if (!val) {
+    upload.value.fileList = []
+  }
+})
 
 // 上传前回调
-const handleBeforeUpload: UploadProps['beforeUpload'] = (file) => {
+const handleBeforeUpload: UploadProps['beforeUpload'] = async (file) => {
   model.value = file
   return false
 }
+
 // 处理删除
 const handleRemove: UploadProps['onRemove'] = () => {
   model.value = null
+  // 避免和上面watch监听冲突
+  return false
 }
 
 const handlePreview = async (file: any) => {
   const url = (await file2base64(file.originFileObj)) as string
-
   imageViewer.value.show(file.name, url)
 }
 
