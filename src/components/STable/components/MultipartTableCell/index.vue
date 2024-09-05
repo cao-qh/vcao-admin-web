56a8fe86779c6c17e4cb584bb2c22e72f7243939
cao-qh <380229607@qq.com>
2024-09-05 14:40:32 +0800

    数据表格组件优化

---
+++ src/components/STable/components/MultipartTableCell/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-flex>
+    <div style="flex: 1; text-align: right; text-wrap: nowrap">
+      <slot name="label"></slot>
+    </div>
+    <a-flex style="flex: 3; text-align: left" vertical align="start">
+      <slot name="value"></slot>
+    </a-flex>
+  </a-flex>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
---
+++ src/components/STable/index.ts
@@ -0,0 +1,4 @@
+import STable from './index.vue'
+import MultipartTableCell from './components/MultipartTableCell/index.vue'
+
+export { STable, MultipartTableCell }
