849c7f6f00fa72493aff0f4de91e30aaf36a8721
cao-qh <380229607@qq.com>
2024-11-10 09:05:20 +0800

    积分兑换，查询，添加，修改，删除接口对接完成

--- src/views/ScoreManager/ScoreExchange/index.vue
+++ src/views/ScoreManager/ScoreExchange/index.vue
@@ -1,7 +1,54 @@
 <template>
-  <PageWrapper>积分管理</PageWrapper>
+  <PageWrapper>
+    <a-space size="large">
+      <span v-if="data">金额：{{ data.jine }}</span>
+      <a-button type="primary" v-if="!data" @click="() => add.show()">
+        添加
+      </a-button>
+      <template v-else>
+        <a-button type="primary" @click="() => edit.show(data)">修改</a-button>
+        <a-button type="primary" @click="handleDelete">删除</a-button>
+      </template>
+    </a-space>
+
+    <Add ref="add" @success="gatData()" />
+
+    <Edit ref="edit" @success="gatData()" />
+  </PageWrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { reqSearch, reqDelete } from '@/api/ScoreManager/ScoreExchange'
+import Add from './modules/Add.vue'
+import Edit from './modules/Edit.vue'
+import { message } from 'ant-design-vue'
 
-<style></style>
+const data = ref<any>(null)
+const add = ref()
+const edit = ref()
+
+onMounted(() => {
+  gatData()
+})
+
+const gatData = async () => {
+  const res = await reqSearch()
+  if (res.code == 0) {
+    data.value = res.data
+  } else {
+    data.value = null
+  }
+}
+
+// 删除
+const handleDelete = async () => {
+  const res = await reqDelete()
+  if (res.code == 0) {
+    message.success(res.msg)
+    gatData()
+  } else {
+    message.error(res.msg)
+  }
+}
+</script>
---
+++ src/views/ScoreManager/ScoreExchange/modules/Add.vue
@@ -0,0 +1,79 @@
+<template>
+  <a-modal
+    title="添加"
+    :open="open"
+    @ok="submit"
+    @cancel="open = false"
+    :body-style="{ maxHeight: '580px', overflow: 'auto' }"
+  >
+    <a-form ref="formRef" :model="formState" :rules="rules" v-bind="layout">
+      <a-form-item label="金额" name="jine">
+        <a-input-number
+          :min="0"
+          v-model:value.trim="formState.jine"
+          placeholder="请输入"
+        />
+      </a-form-item>
+    </a-form>
+  </a-modal>
+</template>
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import { message } from 'ant-design-vue'
+import { reqAdd } from '@/api/ScoreManager/ScoreExchange'
+
+defineOptions({ name: 'Add' })
+
+// 定义方法
+const $emit = defineEmits(['success'])
+
+const open = ref<boolean>(false)
+
+// 表单布局
+const layout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 7 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 13 },
+  },
+}
+
+const formRef = ref()
+const formState = reactive<any>({})
+
+const rules = {
+  jine: [{ required: true, message: '请输入' }],
+}
+
+const show = () => {
+  open.value = true
+  Object.assign(formState, {
+    jine: '',
+  })
+  formRef.value?.clearValidate()
+}
+
+const submit = async () => {
+  try {
+    await formRef.value.validate()
+    console.log('formState :>> ', formState)
+    const res = await reqAdd(formState)
+    if (res.code == 0) {
+      $emit('success')
+      open.value = false
+      message.success(res.msg)
+    } else {
+      message.error(res.msg)
+    }
+  } catch (error) {
+    console.log('error :>> ', error)
+  }
+}
+
+defineExpose({
+  show,
+})
+</script>
